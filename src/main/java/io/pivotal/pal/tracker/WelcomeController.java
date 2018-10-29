6a90a6d883129e875618f14d087d2d2c50220eae
swagatpttnk <swagatpttnk@gmail.com>
2018-10-29 16:33:37 -0400

    Second Lab

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,26 @@
 package io.pivotal.pal.tracker;
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 public class WelcomeController {
 
+    private String hello = "hello";
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return hello;
+    }
+
+
+    public WelcomeController(
+            @Value("${WELCOME_MESSAGE}") String message
+    ) {
+        this.hello = message;
     }
 }
